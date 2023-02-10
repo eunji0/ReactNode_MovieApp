c19e7a5a816602d7e3d065fd067ae807f2a4b22b
eunji <pih9134@naver.com>
2023-02-10 19:51:17 +0900

    mov 2/10

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY = '0e7299cdc96761088e915baf7af097b5'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+
+import React, {useEffect} from 'react'
+import { API_URL, API_KEY } from '../../Config';
 
 function LandingPage() {
+
+    useEffect(()=>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => console.log(response))
+    }, [])
+
     return (
         <div style={{width:'100%', margin:'0'}}>
             {/* {main image} */}
