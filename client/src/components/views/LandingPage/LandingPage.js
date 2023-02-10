336f3c8b48d48a58a31268048d01496fac851b6a
eunji <pih9134@naver.com>
2023-02-11 00:38:25 +0900

    mov 2/11

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,34 @@
 
-import React, {useEffect} from 'react'
-import { API_URL, API_KEY } from '../../Config';
+import React, {useEffect, useState} from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(()=>{
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
         fetch(endpoint)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {
+            console.log(response)
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+        })
     }, [])
 
     return (
         <div style={{width:'100%', margin:'0'}}>
             {/* {main image} */}
+            {MainMovieImage && 
+            <MainImage 
+            image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={MainMovieImage.original_title}
+            text={MainMovieImage.overview}/>
+            }
             <div style={{width:'85%', margin: '1rem auto'}}>
                 <h2>Moview by latest</h2>
                 <hr/>
