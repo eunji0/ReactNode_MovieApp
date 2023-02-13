82ba0f44e39d36cff503b0e238467fa4547d67fa
eunji <pih9134@naver.com>
2023-02-14 00:02:32 +0900

    mov 2/14

--- client/src/components/views/commons/GridCard.js
+++ client/src/components/views/commons/GridCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Col} from 'antd';
 
-function GridCard() {
+function GridCard(props) {
   return (
-    <div>
-      
-    </div>
+    <Col lg={6} md={8} sm={24}>
+        <div style={{position:'relative'}}>
+            <a href={`/movie/${props.movieId}`}>
+                <img style={{width:'100%', height:'320px'}} src={props.image} alt={props.movieName}/>
+            </a>
+        </div>
+    </Col>
   )
 }
 
