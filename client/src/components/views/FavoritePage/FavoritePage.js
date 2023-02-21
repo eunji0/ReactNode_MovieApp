ed7056c1fe2b9da359d1c82a32586a8dd1d36ffe
eunji <pih9134@naver.com>
2023-02-22 00:45:28 +0900

    mov 2/22

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -2,35 +2,64 @@ import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './favorite.css';
 import { Popover } from 'antd';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function FavoritePage() {
 
   const [Favorites, setFavorites] = useState([])
 
   useEffect(() => {
-    Axios.post('/api/favorite/getFavoriteMovie', { userFrom: localStorage.getItem('userId') })
-      .then(response => {
-        if (response.data.success) {
-          console.log(response.data)
-          setFavorites(response.data.favorites)
-        }
-        else {
-          alert('영화 정보를 가져오는데 실패했습니다.')
-        }
-      })
+    fetchFavoriteMovie()
   })
 
-const renderCards = Favorites.map((favorite, index) => {
-  return <tr key={index}>
-    <Popover title={`${favorite.movieTitle}`}>
-    <td>{favorite.movieTitle}</td>
-    </Popover>
-    
-    <td>{favorite.movieRunTime}</td>
-    <td><button>Remove</button></td>
-  </tr>
+  const fetchFavoriteMovie = () => {
+    Axios.post('/api/favorite/getFavoriteMovie', { userFrom: localStorage.getItem('userId') })
+    .then(response => {
+      if (response.data.success) {
+        console.log(response.data)
+        setFavorites(response.data.favorites)
+      }
+      else {
+        alert('영화 정보를 가져오는데 실패했습니다.')
+      }
+    })
+  }
 
-})
+  const renderCards = Favorites.map((favorite, index) => {
+
+    const content = (
+      <div>
+        {favorite.moviePost ?
+
+          <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"
+        }
+      </div>
+    )
+    return <tr key={index}>
+      <Popover content={content} title={`${favorite.movieTitle}`}>
+        <td>{favorite.movieTitle}</td>
+      </Popover>
+
+      <td>{favorite.movieRunTime}</td>
+      <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+    </tr>
+
+  })
+
+  const onClickDelete = (movieId, userFrom) => {
+    const variables = {
+      movieId,userFrom
+    }
+
+    Axios.post('/api/favorite/removeFromFavorite', variables)
+    .then(response => {
+      if(response.data.success){
+        fetchFavoriteMovie()
+      } else{
+        alert('리스트에서 지우는데 실패했습니다.')
+      }
+    })
+  }
 
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
