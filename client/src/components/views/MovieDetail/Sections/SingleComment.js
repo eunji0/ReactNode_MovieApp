18f4b00e06b1d8583a9e7cbb9e28adaab36e1db6
eunji <pih9134@naver.com>
2023-02-27 21:00:56 +0900

    mov 2/27

--- client/src/components/views/MovieDetail/Sections/SingleComment.js
+++ client/src/components/views/MovieDetail/Sections/SingleComment.js
@@ -1,11 +1,27 @@
+import { Avatar, Comment, Button, Input } from 'antd'
 import React from 'react'
 
 function SingleComment() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Comment
+                actions
+                author
+                avatar={<Avatar src alt />}
+                content
+            />
+
+            <form style={{ display: 'flex' }} onSubmit>
+                <textarea style={{ width: '100%', borderRadius: '5px' }}
+                    onChange
+                    value
+                    placeholder="코멘트를 작성해 주세요"
+                />
+                <br />
+                <button style={{ width: '20%', height: '52px' }} onClick>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default SingleComment
